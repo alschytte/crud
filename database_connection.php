7646c28b1041a3c53e777acf960609247e836e1b
Allan Schytt <as@home.dk>
2019-08-19 08:16:49 +0200

    .

--- database_connection.php
+++ database_connection.php
@@ -5,7 +5,7 @@ $DBHOST = getenv('DBHOST');
 $DBUSER = getenv('DBUSER');
 $DBNAME = getenv('DBNAME');
 $DBPASS = getenv('DBPASS');
-$connect = new PDO("mysql:host=$DBHOST;dbname=$DBNAME, $DBUSER, $DBPASS);
+$connect = new PDO("mysql:host=$DBHOST;dbname=$DBNAME", $DBUSER, $DBPASS);
 
 
 
